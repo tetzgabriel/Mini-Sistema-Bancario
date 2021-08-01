f485b2a444b400b23e483f543cbe5a88eae9505e
Gabriel Tetzlaf Mansano <tetz.gabriel@gmail.com>
2021-07-31 23:05:03 -0300

    Feature: Creating Cliente.java

---
+++ src/com/TetzPotz/bank/Cliente.java
@@ -0,0 +1,42 @@
+package com.TetzPotz.bank;
+
+public class Cliente {
+    private int cpf;
+    private String nome;
+
+    protected int[] Extrato;
+
+    public Cliente(int cpf, String nome, int[] extrato) {
+        this.cpf = cpf;
+        this.nome = nome;
+        Extrato = extrato;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int[] getExtrato() {
+        return Extrato;
+    }
+
+    public void setExtrato(int[] extrato) {
+        Extrato = extrato;
+    }
+
+    public int getTamExtrato() {
+        return this.Extrato.length;
+    }
+}
