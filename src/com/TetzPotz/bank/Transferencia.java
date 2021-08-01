0fb57a203d254264beb6d95ffb2250f8f68dd71b
Pedro Potenza <pedro.potenza@unesp.br>
2021-08-01 02:32:02 -0300

    Feature: Transferencia

---
+++ src/com/TetzPotz/bank/Transferencia.java
@@ -0,0 +1,48 @@
+package com.TetzPotz.bank;
+
+public class Transferencia {
+    private int valor;
+    private Conta contaPaga;
+    private Conta contaRecebe;
+
+    public Transferencia(int valor, Conta contaPaga, Conta contaRecebe) {
+        this.valor = valor;
+        this.contaPaga = contaPaga;
+        this.contaRecebe = contaRecebe;
+    }
+
+    @Override
+    public String toString() {
+        return  "Pagante = " + contaPaga.getNome() +
+                "\nConta Pagante = " + contaPaga.getId() +
+                "\nValor transferido = " + (valor>0 ? " + " + valor : " - " + valor) +
+                "\nRecebente =" + contaRecebe.getNome() +
+                "\nConta Recebente =" + contaRecebe.getId() ;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Conta getContaPaga() {
+        return contaPaga;
+    }
+
+    public void setContaPaga(Conta contaPaga) {
+        this.contaPaga = contaPaga;
+    }
+
+
+    public Conta getContaRecebe() {
+        return contaRecebe;
+    }
+
+    public void setContaRecebe(Conta contaRecebe) {
+        this.contaRecebe = contaRecebe;
+    }
+
+}
