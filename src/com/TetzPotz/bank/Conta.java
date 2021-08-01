864737796d62f52e9c9901ba9ceb839411bac18a
Gabriel Tetzlaf Mansano <tetz.gabriel@gmail.com>
2021-07-31 23:17:14 -0300

    Feature: Creating Conta.java

---
+++ src/com/TetzPotz/bank/Conta.java
@@ -0,0 +1,54 @@
+package com.TetzPotz.bank;
+
+import java.util.ArrayList;
+
+public class Conta {
+    private int numeroContas;
+    private int id;
+    private int saldo;
+    private ArrayList<Integer> extrato;
+
+    public Conta(int numeroContas, int id, int saldo, ArrayList<Integer> extrato) {
+        this.numeroContas = numeroContas;
+        this.id = id;
+        this.saldo = saldo;
+        this.extrato = extrato;
+        this.extrato.add(10000);
+    }
+
+    public int getNumeroContas() {
+        return numeroContas;
+    }
+
+    public void setNumeroContas(int numeroContas) {
+        this.numeroContas = numeroContas;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public ArrayList<Integer> getExtrato() {
+        return extrato;
+    }
+
+    public void setExtrato(int valor) {
+        this.extrato.add(valor);
+    }
+
+    public int getTamExtrato() {
+        return this.extrato.size();
+    }
+}
